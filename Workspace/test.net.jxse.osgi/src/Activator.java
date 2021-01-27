aa9ad53b8de224e8b2d6bdb125a8fcda0d63d6bb
info <info@192.168.178.87>
2021-01-27 22:40:34 +0100

    Prepared testsuite

---
+++ Workspace/test.net.jxse.osgi/src/Activator.java
@@ -0,0 +1,21 @@
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext bundleContext;
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+		bundleContext = context;
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		bundleContext = null;
+	}
+
+	public static BundleContext getBundleContext() {
+		return bundleContext;
+	}
+}
---
+++ Workspace/test.net.jxse.osgi/src/test/net/jxse/osgi/suite/TestSuite.java
@@ -0,0 +1,10 @@
+package test.net.jxse.osgi.suite;
+
+public class TestSuite {
+
+	private static TestSuite suite;
+	
+	public static TestSuite getSuite() {
+		return suite;
+	}
+}
